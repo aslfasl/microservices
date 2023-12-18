b1707a9b41e43007f778ab222c3920c467194b64
VolkovAndrey <soupec@yandex.ru>
2023-12-18 19:57:03 +0300

    add k8s, disable eureka

--- fraud-service/src/main/java/ru/hukkucode/fraud/FraudApp.java
+++ fraud-service/src/main/java/ru/hukkucode/fraud/FraudApp.java
@@ -4,10 +4,15 @@ package ru.hukkucode.fraud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class FraudApp {
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
+public class  FraudApp {
     public static void main(String[] args) {
         SpringApplication.run(FraudApp.class, args);
     }
